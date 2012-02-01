408287de619550e5c780d1c179ed5116385033b8
radzior <radoslaw.kloc@gmail.com>
2012-02-01 13:50:18 +0100

    Done with chapter 3

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe PagesController do
 	render_views
+	
+	before (:each) do
+		@base_title = "Ruby on Rails Tutorial Sample App"
+	end
 
   describe "GET 'home'" do
     it "should be successful" do
@@ -11,7 +15,7 @@ describe PagesController do
     it "should have the right title" do
     	get 'home'
     	response.should have_selector("title",
-   			:content => "Ruby on Rails Tutorial Sample App | Home")
+   																	:content => " | Home")
    	end
   end
 
@@ -23,7 +27,7 @@ describe PagesController do
     it "should have the right title" do
     	get 'contact'
     	response.should have_selector("title",
-   			:content => "Ruby on Rails Tutorial Sample App | Contact")
+   																	:content => " | Contact")
    	end
   end
   
@@ -35,8 +39,20 @@ describe PagesController do
     it "should have the right title" do
     	get 'about'
     	response.should have_selector("title",
-   			:content => "Ruby on Rails Tutorial Sample App | About")
+   																	:content => " | About")
    	end
+	end
+	
+	describe "GET 'help'" do
+		it "should be success" do
+			get 'help'
+			response.should be_success
+		end
+		it "should have the right title" do
+			get 'help'
+			response.should have_selector("title",
+																		:content => " | Help")
+		end
 	end
 	
 end
